62c2a85e5a9dc1cf328b098f4a8a10f13dddd100
David Puente <davidpuent@gmail.com>
2026-05-28 23:55:00 +0200

    feat(ui): add TrashIcon and PencilIcon SVG components

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -52,6 +52,22 @@ export const CopyIcon = () => (
 	</svg>
 )
 
+export const TrashIcon = () => (
+	<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+		<title>Delete</title>
+		<polyline points='3 6 5 6 21 6' />
+		<path d='M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2' />
+	</svg>
+)
+
+export const PencilIcon = () => (
+	<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+		<title>Edit</title>
+		<path d='M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7' />
+		<path d='M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z' />
+	</svg>
+)
+
 export const CheckIcon = () => (
 	<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
 		<title>Checkmark Done</title>
